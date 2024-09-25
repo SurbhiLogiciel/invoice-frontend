6ff26abcaa306d7678adaf4f8a829afe1fc505b9
kanan-logiciel <kanan@logiciel.io>
2024-09-25 11:50:36 +0530

    fix: modified setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <h1>Hello  world!</h1>
+      <h1>Hello world!</h1>
     </div>
   );
 }
