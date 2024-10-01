bd91caa4ecfea2315d49a0031aa7897e0cd84b22
RichaLogiciel <159779456+RichaLogiciel@users.noreply.github.com>
2024-10-01 15:18:41 +0530

    feat: icon-added modified code

--- src/layouts/login.tsx
+++ src/layouts/login.tsx
@@ -36,13 +36,13 @@ export const Layout: React.FC = () => {
               Sign-In
             </Button>
           </div>
-          <div className="mt-5 text-white text-center font-bold">
-            Forgot password
+          <div className="mt-5 text-white text-[14px] text-center font-bold">
+            Forgot password?
           </div>
           <div className="mt-[50px]">
             <hr />
           </div>
-          <div className="mt-[30px] text-white text-[18px] text-center">
+          <div className="mt-[30px] text-white text-[18px]">
             Don't have an account?
           </div>
           <div className="mt-[30px]">
@@ -53,12 +53,16 @@ export const Layout: React.FC = () => {
           <div className="mt-[30px]">
             <hr />
           </div>
-          <div className="text-white mt-[30px] text-center">
+          <div className="text-white text-[18px] mt-[30px]">
             Do you need help? Contact technical support.
           </div>
-          <div className="mt-[27px] text-white text-center">
-            <i className="fas fa-info-circle" aria-hidden="true"></i>
-            support@invoice.com
+          <div className="mt-[27px] text-white flex">
+            <span className="mr-2">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.0005 16H14.5005C14.7766 16 15.0005 16.2239 15.0005 16.5C15.0005 16.7761 14.7766 17 14.5005 17H10.5005C10.2243 17 10.0005 16.7761 10.0005 16.5C10.0005 16.2239 10.2243 16 10.5005 16H12.0005V11H10.5005C10.2243 11 10.0005 10.7761 10.0005 10.5C10.0005 10.2239 10.2243 10 10.5005 10H12.5005C12.7766 10 13.0005 10.2239 13.0005 10.5V16ZM12.0005 22C6.47764 22 2.00049 17.5228 2.00049 12C2.00049 6.47715 6.47764 2 12.0005 2C17.5233 2 22.0005 6.47715 22.0005 12C22.0005 17.5228 17.5233 22 12.0005 22ZM12.0005 21C16.9711 21 21.0005 16.9706 21.0005 12C21.0005 7.02944 16.9711 3 12.0005 3C7.02993 3 3.00049 7.02944 3.00049 12C3.00049 16.9706 7.02993 21 12.0005 21ZM11.5005 7H12.5005C12.7766 7 13.0005 7.22386 13.0005 7.5V8.5C13.0005 8.77614 12.7766 9 12.5005 9H11.5005C11.2243 9 11.0005 8.77614 11.0005 8.5V7.5C11.0005 7.22386 11.2243 7 11.5005 7Z" fill="#FF5E85"/>
+              </svg>
+            </span>
+            <span>support@invoice.com</span>
           </div>
         </div>
         <div className="bg-secondary mx-auto w-full p-4 rounded-lg">
