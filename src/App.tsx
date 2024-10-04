a099654b8be754b829e994323a3db7158a13b9a3
RichaLogiciel <159779456+RichaLogiciel@users.noreply.github.com>
2024-10-04 14:47:33 +0530

    fix: removed paid chip component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-// import { Button } from './core-ui/button';
-import { PaidChip } from './core-ui/chips';
 
 import './index.css'; 
 
 const App: React.FC = () => {
   return (
     <>
-      <PaidChip outline size="small" color="danger" onClick={() => console.log('clicked')}>
-        Invoice Paid
-      </PaidChip>
     </>
   );
 };
