adc153064db8e4b431152dfc0bea08f31abae251
RichaLogiciel <159779456+RichaLogiciel@users.noreply.github.com>
2024-10-09 14:36:03 +0530

    Revert "Feature/UI inputs"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Invoice from './components/invoice';
 
 function App() {
   return (
     <div className="App">
-      <Invoice />
+      <h1>Hello world!</h1>
     </div>
   );
 }
