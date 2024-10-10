dc0c17b66ec7948b1b9227881533c2e7a938e983
RichaLogiciel <159779456+RichaLogiciel@users.noreply.github.com>
2024-10-10 16:39:44 +0530

    feat: modified setup

--- src/core-ui/choosePlan/types.ts
+++
@@ -1,9 +0,0 @@
-export type SelectableContainerColor = 'primary' | 'danger';
-export type SelectableContainerSize = 'small' | 'medium' | 'large';
-
-export interface SelectableContainerProps {
-  outline?: boolean;
-  color?: SelectableContainerColor;
-  size?: SelectableContainerSize;
-  children: React.ReactNode;
-}
