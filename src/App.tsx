711f0e035f03d13741e10830076b03c875a03d92
RichaLogiciel <159779456+RichaLogiciel@users.noreply.github.com>
2024-10-10 12:35:33 +0530

    feat: tsx file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './App.css';
+
+import './index.css';
 
 const App: React.FC = () => {
-
   return (
-    <>
-    </>
-   
+    <div className="App">
+      <h1>Hello world!</h1>
+    </div>
   );
 };
 
